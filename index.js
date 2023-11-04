ed860ac4bf026f244ab3d817af4a6163606f01a1
andresaraque <andresaraque24dos@gmail.com>
2023-11-04 00:46:37 -0500

    :construction: initial configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+
+
+//Default path
+app.get("/", (req, res) => {
+  res.send("All good");
+});
+
+app.listen(port, () => {
+  console.log(`Server running in port: ${port}`);
+});
